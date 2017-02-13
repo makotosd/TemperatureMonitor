3d0ff7876215914e97c075c0dd3503eb9e0ed810
makotosd <mouri.makoto@gmail.com>
2017-02-14 00:21:33 +0900

    temp or humidity is selectable

--- tempgraph.js
+++ tempgraph.js
@@ -1,6 +1,9 @@
-    
+function tempgraph(){    
 //Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/wind_speed_laurel_nebraska.csv', function(rows){
 Plotly.d3.csv('http://192.168.1.15/temperature/resultq.csv', function(rows){
+
+    var form = document.forms.param;
+
     var viewdate = ['2017/02/06', '2017/02/07', '2017/02/08'];
 
     var tracel = viewdate.map(function(vd){
@@ -17,7 +20,11 @@ Plotly.d3.csv('http://192.168.1.15/temperature/resultq.csv', function(rows){
 		return ret.replace(/:[0-9][0-9]$/, "");
 	    }),
 	    y: filtered.map(function(row){          // set the x-data
-		return row['temperature'];
+		if(form._th.value == 'temperature'){
+		    return row['temperature'];
+		}else{
+		    return row['humidity'];
+		}
 	    }),
 	    line: {                             // set the width of the line.
 		width: 1
@@ -27,15 +34,18 @@ Plotly.d3.csv('http://192.168.1.15/temperature/resultq.csv', function(rows){
     });
 
     var layout = {
-      yaxis: {title: "temp"},       // set the y axis title
-      xaxis: {
-        showgrid: false,                  // remove the x-axis grid lines
-        tickformat: "%B, %Y"              // customize the date format to "month, day"
-      },
-      margin: {                           // update the left, bottom, right, top margin
-        l: 40, b: 100, r: 10, t: 20
-      }
+	yaxis: {
+	    title: form._th.value
+	},
+	xaxis: {
+            showgrid: false,                  // remove the x-axis grid lines
+            tickformat: "%B, %Y"              // customize the date format to "month, day"
+	},
+	margin: {                           // update the left, bottom, right, top margin
+            l: 40, b: 100, r: 10, t: 20
+	}
     };
 
-    Plotly.plot(document.getElementById('temperature'), tracel, layout, {showLink: false});
+    Plotly.newPlot(document.getElementById('temperature'), tracel, layout, {showLink: false});
 });
+};
